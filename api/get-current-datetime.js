00868e96f3045087d3708fae5f580956eea6b6da
demoaiagent1-ux <demoaiagent1@gmail.com>
2025-11-13 00:33:08 +0100

    Update get-current-datetime.js

--- api/get-current-datetime.js
+++ api/get-current-datetime.js
@@ -1,13 +1,23 @@
 export default function handler(req, res) {
+  // Preflight OPTIONS
+  if (req.method === 'OPTIONS') {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+    return res.status(200).end();
+  }
+
   const now = new Date();
   const currentDate = now.toLocaleDateString("nl-NL", { timeZone: "Europe/Amsterdam" });
   const currentTime = now.toLocaleTimeString("nl-NL", { timeZone: "Europe/Amsterdam" });
 
-  // Voeg CORS header toe zodat VAPI verbinding kan maken
+  // CORS header
   res.setHeader('Access-Control-Allow-Origin', '*');
+  // Zorg dat Content-Type expliciet JSON is
+  res.setHeader('Content-Type', 'application/json');
 
-  res.status(200).json({
+  res.status(200).send(JSON.stringify({
     currentDate,
     currentTime
-  });
+  }));
 }
